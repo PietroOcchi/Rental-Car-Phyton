08f267221c3c67ede2adf863006dc9a69b07d56a
Pietro Occhi <pietrosocchi@gmail.com>
2025-10-16 20:17:19 -0300

    Adicionado cadastro de veículos e melhorado e melhorias no título

---
+++ Application.py
@@ -0,0 +1,71 @@
+import os
+
+####### FUNÇÕES GLOBAIS
+def limpar_tela():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+def pressione_continuar():
+    input("Pressione Enter para continuar...")
+
+
+####### TITULO DO SISTEMA
+def titulo():
+    limpar_tela()
+    print("#"*50)
+    print("#"+ " "*15 +"LOCADORA DE CARROS" + " "*15+"#")
+    print("#"*50)
+
+####### MENU PRINCIPAL
+def menu():
+    print("\n#Menu de Opções")
+    print("1 - Cadastro de Clientes")
+    print("2 - Cadastro de Veículos")
+    print("3 - Alugar Carro")
+    print("4 - Sair do Sistema")
+
+####### FUNÇÕES DO SISTEMA
+def cadastro_clientes():
+    nome = input("Nome: ")
+    ano_nascimento = int(input("Ano de nascimento: "))
+    email = input("E-mail: ")
+
+def cadastro_veiculos():
+    modelo = input("Nome: ")
+    ano = int(input("Ano: "))
+    cor = input("Cor: ")
+
+
+while True:
+    limpar_tela()
+    titulo()
+    menu()
+
+    opcao=int(input("\nInforme uma opção: "))
+
+    match opcao:
+        case 1:
+            titulo()
+            print("\n#### CADASTRO DE CLIENTES ####")
+            cadastro_clientes()
+            pressione_continuar()
+
+        case 2:
+            titulo()
+            print("\n#CADASTRO DE VEÍCULOS")
+            cadastro_veiculos()
+            pressione_continuar()
+
+        case 3:
+            print("\n#ALUGAR CARRO")
+            pressione_continuar()
+
+        case 4:
+            print("\n#SAIR")
+            
+        case 5:
+            print("\n#OPÇÃO INVÁLIDA")
+            pressione_continuar()
+
+
+
+
